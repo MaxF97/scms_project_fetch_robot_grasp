9b73eeba3bc87bf7a48ef1dbda4ad411e6b3bf79
samuelGallagher <samuel1j1@gmail.com>
2020-06-05 15:51:16 +1000

    Minor Sequence Change

--- assignment/src/builder.py
+++ assignment/src/builder.py
@@ -46,19 +46,34 @@ if __name__ == "__main__":
         if(counter == 3):
             #print(counter)
             print("yes")
-            Arm.OriginReturn(Ros.x, Ros.y, Ros.z, Ros.a, Ros.b, Ros.c, Ros.d)
+            Arm.MoveToPose(Ros.x, Ros.y, (Ros.z+0.2), Ros.a, Ros.b, Ros.c, Ros.d)
             counter+=1
         if(counter == 4):
             print("hahha")
-            Arm.OriginPart2(Ros.x, Ros.y, Ros.z, Ros.a, Ros.b, Ros.c, Ros.d)
+            Arm.MoveToPose(0.35, 0.37, (0.2+Ros.z), Ros.a, Ros.b, Ros.c, Ros.d)
             counter+=1
 
         if(counter == 5):
             print ("oh no")
-            Arm.OriginPart3(Ros.x, Ros.y, Ros.z, Ros.a, Ros.b, Ros.c, Ros.d)
-            counter+=1
+            Arm.MoveToPose(0.35, 0.37, 0.3, Ros.a, Ros.b, Ros.c, Ros.d)
+            counter=+=1
+            #Stop here to test Camera
+            #Needs to go Down and then up again
         if(counter == 6):
             print("yesterday Today")
+            Arm.MoveToPose(0.35, 0.37, Ros.z, Ros.a, Ros.b, Ros.c, Ros.d)
+            counter+=1
+        if(counter == 7):
+            print("I'm Tired")
             Arm.MoveToPose(0.708, -0.026, 0.9, Ros.a, Ros.b, Ros.c, Ros.d)
+            counter+=1
+        if(counter ==8):
+            print("Drop the Cube")
+            GripHand.GripOpen()
+            counter +=1
+
+
+
+
 
         
