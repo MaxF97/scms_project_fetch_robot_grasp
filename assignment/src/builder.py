700d9d4b14c9f3e92ee2a1094bc6b18706911a5c
samuelGallagher <samuel1j1@gmail.com>
2020-06-05 10:39:03 +1000

    Gripper Update

--- assignment/src/builder.py
+++ assignment/src/builder.py
@@ -4,6 +4,8 @@ import rospy
 
 from robot_arm import RobotArm
 from ROSInterface import ROSInterface
+from gripper import Gripper
+counter = 0
 
 
 
@@ -11,12 +13,15 @@ if __name__ == "__main__":
     # Setup clients
     Ros = ROSInterface()
     Arm = RobotArm()
+    GripHand = Gripper()
+    grip_state = 0
     while 1:
         try:
             Ros.Subscriber()
             break
         except:
             rospy.loginfo('waiting for matlab')
+            print(counter)
 
     
     while not rospy.is_shutdown():
@@ -29,6 +34,18 @@ if __name__ == "__main__":
             if Arm.CheckGoal() == 1:
                 Ros.GetCheck(1)
                 Ros.callback = 0
+                grip_state = 1
+                counter+=1
+                print(counter)
             else:
                 Ros.GetCheck(0)
+        if(counter == 2):
+            GripHand.GripClose()
+            GripHand.GripOpen()
+            counter+=1
+        if(counter == 1):
+            print(counter)
+        if(counter == 2):
+            print(counter)
+            print("yes")
         
