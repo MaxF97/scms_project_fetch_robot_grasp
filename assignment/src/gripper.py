cd89499542075ee45974e02508be05167340a168
samuelGallagher <samuel1j1@gmail.com>
2020-06-05 10:35:43 +1000

    Gripper Update

---
+++ assignment/src/gripper.py
@@ -0,0 +1,27 @@
+
+import rospy
+
+import actionlib
+import control_msgs.msg
+import sys
+import time
+
+ACTION_SERVER = 'gripper_controller/gripper_action'
+class Gripper:
+    def __init__(self):
+        #effort_min = 30
+        #effort_max = 100
+        self._client = actionlib.SimpleActionClient(ACTION_SERVER, control_msgs.msg.GripperCommandAction)
+        self._client.wait_for_server(rospy.Duration(10))
+    def GripOpen(self):
+        goal = control_msgs.msg.GripperCommandGoal()
+        goal.command.position = 0.10
+        self._client.send_goal_and_wait(goal, rospy.Duration(10))
+
+
+    def GripClose(self):
+        effort_max = 100
+
+        goal = control_msgs.msg.GripperCommandGoal()
+        goal.command.max_effort = effort_max
+        self._client.send_goal_and_wait(goal, rospy.Duration(1))
